4380bc9b549e49e69be6d0b10122a68001d53d72
码云 <1017719268@qq.com>
2026-01-22 18:11:36 +0800

    v2.2.1 ws优化，日志放入缓存，后续队列发送。二次打开无法中断任务bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,7 +21,6 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
-    allowedHosts: ['fnos.chrmjj.fun'],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
