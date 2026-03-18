7a1e5d45e62e3a31bcd9a2f27ce7f65e4d7c1bbf
码云 <1017719268@qq.com>
2026-03-18 11:04:48 +0800

    Revert "Revert "feat: 添加 workflow 子工作流支持""
    This reverts commit 936b879e

--- backend/app/modules/workflow/schemas.py
+++ backend/app/modules/workflow/schemas.py
@@ -116,6 +116,7 @@ class NodeTypes:
     PARALLEL = "parallel"
     WAIT = "wait"
     NOTIFICATION = "notification"
+    SUBWORKFLOW = "subworkflow"  # 子工作流
 
 
 # ============== 工作流版本 ==============
