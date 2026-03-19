c2445b17a6c5292aa54c09f203060e397d135017
码云 <1017719268@qq.com>
2026-03-19 15:05:14 +0800

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
+    allowedHosts: ['chrmjj.fun','fnos.chrmjj.fun','chrmjj.fnos.net'],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
